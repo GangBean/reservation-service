015583bb640d9867c63e8b25359dd51804dd440a
Gangbean <xbox2537@gmail.com>
2023-06-15 09:48:17 +0900

    Fix: ProfileController

--- src/main/java/com/gangbean/reservationservice/controller/ProfileController.java
+++ src/main/java/com/gangbean/reservationservice/controller/ProfileController.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class ProfileController {
 
     private final Environment environment;
